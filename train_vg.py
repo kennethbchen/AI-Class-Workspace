76e12294d6a2c3420fbd23c9cd884693699e88ea
kennethbchen <kennethbchen@gmail.com>
2023-09-25 11:16:25 -0400

    Made model train with 3 numeric parameters

--- train_vg.py
+++ train_vg.py
@@ -61,7 +61,7 @@ genre_vocab = torchtext.vocab.build_vocab_from_iterator([genre_tokens], specials
 #genre_one_hot = nn.functional.one_hot(torch.tensor(genre_vocab.forward(genre_tokens)), num_classes=len(genre_vocab))
 
 # TODO: get genre_one_hot in with the rest of the input_data somehow
-genre_one_hot = build_one_hot(genre_vocab, list(processed_data["Genre"].values))
+#genre_one_hot = build_one_hot(genre_vocab, list(processed_data["Genre"].values))
 
 
 data_true = processed_data["Global_Sales"]
@@ -69,33 +69,27 @@ input_data = processed_data[["Critic_Score", "Critic_Count", "User_Score"]]
 
 #print(torch.index_select(genre_one_hot, 0, torch.tensor([1, 2, 3, 4], dtype=torch.int32)))
 
-
-
-
-kf = KFold(n_splits=5, shuffle=True)
+kf = KFold(n_splits=5, shuffle=False)
 epochs = 1000
 
-
 #print(torch.nn.functional.one_hot(torch.tensor(df["Genre"].values), get_class_count(df["Genre"].values)))
-
 
 print("Start Training")
 for fold, (train_indexes, test_indexes) in enumerate(kf.split(input_data)):
 
-    model = nn.Linear(1, 1)
+    model = nn.Linear(3, 1)
     loss = nn.MSELoss()
     optimizer = torch.optim.SGD(model.parameters(), lr=0.0001)
     model.train()
     for epoch in range(epochs):
         optimizer.zero_grad()
 
-        # https://pytorch.org/docs/stable/generated/torch.reshape.html#torch.reshape
-        y_pred = model(torch.index_select(genre_one_hot, 0, torch.tensor(train_indexes, dtype=torch.int32)))
-        print(y_pred)
-        exit()
-        #y_pred = torch.reshape(y_pred, (-1, ))
 
-        y_true = torch.tensor(data_true[train_indexes].values, dtype=torch.float32)
+        # https://pytorch.org/docs/stable/generated/torch.reshape.html#torch.reshape
+        y_pred = model(torch.tensor(input_data.iloc[train_indexes].values, dtype=torch.float32).unsqueeze(1))
+        y_pred = torch.reshape(y_pred, (-1, ))
+
+        y_true = torch.tensor(data_true.iloc[train_indexes].values, dtype=torch.float32)
 
         epoch_loss = loss(y_pred, y_true)
 
@@ -108,7 +102,7 @@ for fold, (train_indexes, test_indexes) in enumerate(kf.split(input_data)):
     y_pred = model(torch.tensor(input_data.iloc[test_indexes].values, dtype=torch.float32))
     y_pred = torch.reshape(y_pred, (-1,))
 
-    y_true = torch.tensor(data_true[test_indexes].values, dtype=torch.float32)
+    y_true = torch.tensor(data_true.iloc[test_indexes].values, dtype=torch.float32)
 
     print("Loss:", loss(y_pred, y_true))
     print()
