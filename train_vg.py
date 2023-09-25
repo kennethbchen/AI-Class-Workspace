b3a8c85a3879288788e887827c6791767af14d6e
kennethbchen <kennethbchen@gmail.com>
2023-09-25 11:21:38 -0400

    Rename input_data to numeric_data

--- train_vg.py
+++ train_vg.py
@@ -60,12 +60,12 @@ genre_vocab = torchtext.vocab.build_vocab_from_iterator([genre_tokens], specials
 
 #genre_one_hot = nn.functional.one_hot(torch.tensor(genre_vocab.forward(genre_tokens)), num_classes=len(genre_vocab))
 
-# TODO: get genre_one_hot in with the rest of the input_data somehow
+# TODO: get genre_one_hot in with the rest of the numeric_data somehow
 #genre_one_hot = build_one_hot(genre_vocab, list(processed_data["Genre"].values))
 
 
 data_true = processed_data["Global_Sales"]
-input_data = processed_data[["Critic_Score", "Critic_Count", "User_Score"]]
+numeric_data = processed_data[["Critic_Score", "Critic_Count", "User_Score"]]
 
 #print(torch.index_select(genre_one_hot, 0, torch.tensor([1, 2, 3, 4], dtype=torch.int32)))
 
@@ -75,7 +75,7 @@ epochs = 1000
 #print(torch.nn.functional.one_hot(torch.tensor(df["Genre"].values), get_class_count(df["Genre"].values)))
 
 print("Start Training")
-for fold, (train_indexes, test_indexes) in enumerate(kf.split(input_data)):
+for fold, (train_indexes, test_indexes) in enumerate(kf.split(numeric_data)):
 
     model = nn.Linear(3, 1)
     loss = nn.MSELoss()
@@ -86,7 +86,7 @@ for fold, (train_indexes, test_indexes) in enumerate(kf.split(input_data)):
 
 
         # https://pytorch.org/docs/stable/generated/torch.reshape.html#torch.reshape
-        y_pred = model(torch.tensor(input_data.iloc[train_indexes].values, dtype=torch.float32).unsqueeze(1))
+        y_pred = model(torch.tensor(numeric_data.iloc[train_indexes].values, dtype=torch.float32).unsqueeze(1))
         y_pred = torch.reshape(y_pred, (-1, ))
 
         y_true = torch.tensor(data_true.iloc[train_indexes].values, dtype=torch.float32)
@@ -99,7 +99,7 @@ for fold, (train_indexes, test_indexes) in enumerate(kf.split(input_data)):
 
     print("-------- Fold", fold, "--------")
     model.eval()
-    y_pred = model(torch.tensor(input_data.iloc[test_indexes].values, dtype=torch.float32))
+    y_pred = model(torch.tensor(numeric_data.iloc[test_indexes].values, dtype=torch.float32))
     y_pred = torch.reshape(y_pred, (-1,))
 
     y_true = torch.tensor(data_true.iloc[test_indexes].values, dtype=torch.float32)
