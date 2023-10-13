41dcb8417967ff2a2c6dfe405eb528d307e6973d
kennethbchen <kennethbchen@gmail.com>
2023-10-13 11:27:19 -0400

    Started using a sequential model and implemented cuda support

--- train_vg.py
+++ train_vg.py
@@ -16,7 +16,6 @@ from gensim.models import KeyedVectors
     Data Processing On CSV: Remove "tbd" from User_Score / all rows
 """
 
-print("Loading Dataset")
 df = pd.read_csv("data/video_games_sales.csv")
 df["User_Score"] = df["User_Score"].astype(float)
 df["User_Count"] = df["User_Count"].astype(float)
@@ -38,6 +37,7 @@ Developer Rating
 Predict: Global_Sales
 
 """
+
 
 processed_data = df.dropna()
 
@@ -75,14 +75,29 @@ def read_word_embeddings():
 
     return name_embeddings
 
+# Checks if GPU can be used
+# https://pytorch.org/get-started/locally/
+if torch.cuda.is_available():
+    print("Cuda is available")
+    device = torch.device("cuda:0")
+else:
+    device = torch.device("cpu")
+
+
 data_true = processed_data["Global_Sales"]
+
 numeric_data = processed_data[["Critic_Score", "Critic_Count", "User_Score"]]
+# Normalize
+numeric_data = ( (numeric_data - numeric_data.mean()) / numeric_data.std() )
+
 one_hot_dummies_data = pandas.get_dummies(processed_data[["Platform", "Genre"]], prefix=["Platform", "Genre"])
 name_embeddings = np.array(read_csv_cached("ignore/cached_data/name_embeddings.csv", read_word_embeddings)).astype(float)
 
-input_data = torch.cat([torch.tensor(name_embeddings), torch.tensor(one_hot_dummies_data.values), torch.tensor(numeric_data.values)], 1)
-print("Input size:", len(input_data), "rows")
-print("Row size:", len(input_data[0]), "values")
+input_data = torch.cat([torch.tensor(name_embeddings), torch.tensor(one_hot_dummies_data.values), torch.tensor(numeric_data.values)], 1).to(device)
+input_size = input_data.shape[1]
+
+print("Dataset size:", len(input_data), "rows")
+print("Input size:", input_size, "values")
 print("Input sample:", input_data[0])
 
 kf = KFold(n_splits=5, shuffle=True)
@@ -93,20 +108,32 @@ fold_losses = []
 print("Start Training")
 for fold, (train_indexes, test_indexes) in enumerate(kf.split(numeric_data)):
 
-    model = nn.Linear(input_data.shape[1], 1)
+
+    model = nn.Sequential(
+        nn.Linear(input_size, 100),
+        nn.ReLU(),
+        nn.Linear(100, 100),
+        nn.ReLU(),
+        nn.Linear(100, 100),
+        nn.ReLU(),
+        nn.Linear(100, 100),
+        nn.ReLU(),
+        nn.Linear(100, 1)
+    ).to(device)
+
     loss = nn.MSELoss()
     optimizer = torch.optim.SGD(model.parameters(), lr=0.0001)
     model.train()
     for epoch in range(epochs):
         optimizer.zero_grad()
 
-        epoch_input = torch.index_select(input_data, 0, torch.tensor(train_indexes, dtype=torch.int32)).type(torch.float32)
+        epoch_input = torch.index_select(input_data, 0, torch.tensor(train_indexes, dtype=torch.int32).to(device)).type(torch.float32)
 
         # https://pytorch.org/docs/stable/generated/torch.reshape.html#torch.reshape
         y_pred = model(epoch_input)
         y_pred = torch.reshape(y_pred, (-1, ))
 
-        y_true = torch.tensor(data_true.iloc[train_indexes].values, dtype=torch.float32)
+        y_true = torch.tensor(data_true.iloc[train_indexes].values, dtype=torch.float32).to(device)
 
         epoch_loss = loss(y_pred, y_true)
 
@@ -117,11 +144,11 @@ for fold, (train_indexes, test_indexes) in enumerate(kf.split(numeric_data)):
     print("-------- Fold", fold, "--------")
     model.eval()
 
-    test_input = torch.index_select(input_data, 0, torch.tensor(test_indexes, dtype=torch.int32)).type(torch.float32)
+    test_input = torch.index_select(input_data, 0, torch.tensor(test_indexes, dtype=torch.int32).to(device)).type(torch.float32).to(device)
     y_pred = model(test_input)
     y_pred = torch.reshape(y_pred, (-1,))
 
-    y_true = torch.tensor(data_true.iloc[test_indexes].values, dtype=torch.float32)
+    y_true = torch.tensor(data_true.iloc[test_indexes].values, dtype=torch.float32).to(device)
 
     test_loss = loss(y_pred, y_true)
     fold_losses.append(test_loss)
