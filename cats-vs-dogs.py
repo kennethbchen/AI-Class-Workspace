c60a60696d2aa02f7ee69a203e45e2bdf84c89bf
kennethbchen <kennethbchen@gmail.com>
2023-11-27 10:32:21 -0500

    Implemented using resnet and fine tuning

--- cats-vs-dogs.py
+++ cats-vs-dogs.py
@@ -4,76 +4,94 @@ from torch import nn
 from torchvision import transforms
 import torchsummary
 
-img_dimensions = 224
-batch_size = 32
+def main():
+    # Checks if GPU can be used
+    # https://pytorch.org/get-started/locally/
+    if torch.cuda.is_available():
+        print("Cuda is available")
+        device = torch.device("cuda:0")
+    else:
+        device = torch.device("cpu")
 
-# Define the transformations to be applied to the images
-img_transforms = transforms.Compose([
-    transforms.Resize((img_dimensions, img_dimensions)),
-    transforms.ToTensor()
-])
+    img_dimensions = 224
+    batch_size = 512
 
-# Load the dataset
-dataset = torchvision.datasets.ImageFolder(
-    root="ignore/cats-vs-dogs",
-    transform=img_transforms
-)
+    # Define the transformations to be applied to the images
+    img_transforms = transforms.Compose([
+        transforms.Resize((img_dimensions, img_dimensions)),
+        transforms.ToTensor()
+    ])
 
-# Split the dataset into training and validation sets
-train_dataset, val_dataset = torch.utils.data.random_split(dataset, [20000, 5000])
+    print("Load dataset")
+    # Load the dataset
+    dataset = torchvision.datasets.ImageFolder(
+        root="ignore/cats-vs-dogs",
+        transform=img_transforms
+    )
 
-# Create training and validation dataloaders
-train_dataloader = torch.utils.data.DataLoader(
-    train_dataset,
-    batch_size=batch_size,
-    num_workers=0,
-    shuffle=True,
-)
+    # Split the dataset into training and validation sets
+    train_dataset, val_dataset = torch.utils.data.random_split(dataset, [20000, 5000])
 
-val_dataloader = torch.utils.data.DataLoader(
-    val_dataset,
-    batch_size=batch_size,
-    num_workers=0,
-    shuffle=True
-)
+    # Create training and validation dataloaders
+    train_dataloader = torch.utils.data.DataLoader(
+        train_dataset,
+        batch_size=batch_size,
+        num_workers=2,
+        shuffle=True,
+    )
 
-model = nn.Sequential(
-    nn.Conv2d(3, 16, kernel_size=3, padding=1),
-    nn.ReLU(),
-    nn.MaxPool2d(2, 2),
-    nn.Conv2d(16, 32, kernel_size=3, padding=1),
-    nn.ReLU(),
-    nn.MaxPool2d(2, 2),
-    nn.Conv2d(32, 64, kernel_size=3, padding=1),
-    nn.ReLU(),
-    nn.MaxPool2d(2, 2),
-    nn.Flatten(),
-    nn.Linear(64 * 28 * 28, 512),
-    nn.ReLU(),
-    nn.Linear(512, 2)
-)
+    val_dataloader = torch.utils.data.DataLoader(
+        val_dataset,
+        batch_size=batch_size,
+        num_workers=2,
+        shuffle=True
+    )
 
-#torchsummary.summary(model, (3, img_dimensions, img_dimensions), device="cpu")
+    model = torchvision.models.resnet50(pretrained=True)
 
-optim = torch.optim.Adam(model.parameters(), lr=0.001)
-loss_fn = nn.CrossEntropyLoss()
+    for param in model.parameters():
+        param.requires_grad = False
 
-print("Start Training")
-for epoch in range(10):
-    for batch in train_dataloader:
-        x, y = batch
-        y_hat = model(x)
-        loss = loss_fn(y_hat, y)
-        optim.zero_grad()
-        loss.backward()
-        optim.step()
-    print(f"Epoch {epoch} loss: {loss}")
-    correct = 0
-    total = 0
-    for batch in val_dataloader:
-        x, y = batch
-        y_hat = model(x)
-        _, predicted = torch.max(y_hat.data, 1)
-        total += y.size(0)
-        correct += (predicted == y).sum().item()
-    print(f"Epoch {epoch} accuracy: {correct / total}")
+    model.fc = nn.Linear(2048, 2)
+    model.to(device)
+
+    #torchsummary.summary(model, (3, img_dimensions, img_dimensions), device="cpu")
+
+    optim = torch.optim.Adam(model.parameters(), lr=0.001)
+    loss_fn = nn.CrossEntropyLoss()
+
+    print("Start Training")
+    for epoch in range(10):
+
+        for batch in train_dataloader:
+            x, y = batch
+            x = x.to(device)
+            y = y.to(device)
+
+            y_hat = model(x)
+            y_hat.to(device)
+
+            loss = loss_fn(y_hat, y)
+
+            optim.zero_grad()
+            loss.backward()
+            optim.step()
+            print("Batch Complete")
+        print(f"Epoch {epoch} loss: {loss}")
+        print("Testing Epoch accuracy")
+        correct = 0
+        total = 0
+        for batch in val_dataloader:
+            x, y = batch
+            x = x.to(device)
+            y = y.to(device)
+            y_hat = model(x)
+            y_hat.to(device)
+            _, predicted = torch.max(y_hat.data, 1)
+            total += y.size(0)
+            correct += (predicted == y).sum().item()
+        print(f"Epoch {epoch} accuracy: {correct / total}")
+
+
+if __name__ == '__main__':
+    main()
